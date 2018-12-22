38d4b8716ab33183d4da43da9db823fdab380d5b
Kouta3366 <patukin4040@gmail.com>
2018-12-22 10:26:26 +0900

    2

--- words-app/app/models/card.rb
+++ words-app/app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ApplicationRecord
-  validates :word, {presence:true, length:{maximum: 6}}
-  validates :meaning, {presence:true, length:{maximum: 40}}
+  validates :word, {presence:true, length:{maximum: 28}}
+  validates :meaning, {presence:true, length:{maximum: 44}}
 end
